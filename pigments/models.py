e37116d650c1be030fbc420caa6c011227ac22f0
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-22 01:16:31 +0900

    try out some pigments and grains

--- pigments/models.py
+++ pigments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Iwaenogu(models.Model):
+    name_en = models.CharField(max_length=50)
+    name_ja = models.CharField(max_length=50)
+
+class Grain(models.Model):
+    question = models.ForeignKey(Iwaenogu, on_delete=models.CASCADE)
+    size = models.IntegerField(default=0)
+    hex_code = models.CharField(max_length=6)
+    price = models.IntegerField(default=0)
