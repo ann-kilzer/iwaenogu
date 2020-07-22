60418f34b1495bfc450d320d171e59ddca1a0f45
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-22 22:32:42 +0900

    pigment type enum

--- pigments/models.py
+++ pigments/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Iwaenogu(models.Model):
+class Enogu(models.Model):
     name_en = models.CharField(max_length=50)
     name_ja = models.CharField(max_length=50)
+
+  class PigmentType(models.TextChoices):
+    TENNEN_IWA = 'TI', _('Tennen Iwaenogu 天淵岩絵具')
+    SHIN_IWA = 'SI', _('Shin Iwaenogu　新岩絵具')
+    KYOJYO_IWA = 'KI', _('Kyojyo Iwaenogu')
+    GOFUN = 'SG', _('Suihui Gofun')
+    SUIHUI = 'HH', _('Suihui Enogu')
+    TENNEN_TSUCHI = 'TT', _('Tennen Tsuchi Enogu　天淵土絵具')
+
+    pigment_type = models.CharField(
+      max_length=2,
+      choices=PigmentType.choices)
 
 class Grain(models.Model):
     question = models.ForeignKey(Iwaenogu, on_delete=models.CASCADE)
