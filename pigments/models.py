ed93b35a2d8abafa0290ccc181c4ecca5443ac16
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-23 19:41:26 +0900

    string func

--- pigments/models.py
+++ pigments/models.py
@@ -4,10 +4,13 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 class Color(models.Model):
-    name_en = models.CharField(max_length=50)
-    name_ja = models.CharField(max_length=50)
-    description_en = models.CharField(max_length=200)
-    hex_code = models.CharField(max_length=6)
+  name_en = models.CharField(max_length=50)
+  name_ja = models.CharField(max_length=50)
+  description_en = models.CharField(max_length=200)
+  hex_code = models.CharField(max_length=6)
+
+  def __str__(self):
+    return '{0} {1}'.format(self.name_ja, self.name_en) 
 
 class Pigment(models.Model):
   color = models.ForeignKey(Color, on_delete=models.PROTECT)
@@ -35,11 +38,12 @@ class Pigment(models.Model):
     ELEVEN = '11'
     TWELVE = '12'
     THIRTEEN = '13'
-    BYAKU = '白'
+    BYAKU = '白' 
 
   grain = models.CharField(
     max_length=2,
     choices=Grain.choices,
     default=Grain.TWELVE)
 
-
+  def __str__(self):
+    return '{0} {1} {2}'.format(self.pigment_type, self.color, self.grain) 
