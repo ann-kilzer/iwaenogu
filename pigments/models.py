0a6e2f14448bbe4f6df162879436db106b809e18
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-23 19:47:29 +0900

    adjust print

--- pigments/models.py
+++ pigments/models.py
@@ -17,7 +17,7 @@ class Pigment(models.Model):
   hex_code = models.CharField(max_length=6)
   price = models.IntegerField(default=0)
 
-  class PigmentType(models.TextChoices):
+  class Type(models.TextChoices):
     TENNEN_IWA = 'TI', _('Tennen Iwaenogu 天然岩絵具')
     SHIN_IWA = 'SI', _('Shin Iwaenogu　新岩絵具')
     GOFUN = 'GO', _('Gofun 胡粉')
@@ -26,7 +26,7 @@ class Pigment(models.Model):
 
   pigment_type = models.CharField(
     max_length=2,
-    choices=PigmentType.choices)
+    choices=Type.choices)
 
   class Grain(models.TextChoices):
     FIVE = '5'
@@ -46,4 +46,4 @@ class Pigment(models.Model):
     default=Grain.TWELVE)
 
   def __str__(self):
-    return '{0} {1} {2}'.format(self.pigment_type, self.color, self.grain) 
+    return '{0} {1} {2}'.format(self.pigment_type.label, self.color, self.grain) 
