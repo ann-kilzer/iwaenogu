38aadbbb35a334c3d07b982f2d0db59a199d8d51
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-23 22:47:27 +0900

    views

--- iwaenogu/urls.py
+++ iwaenogu/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('pigments/', include('pigments.urls')),
+    path('colors/', include('pigments.urls')),
     path('admin/', admin.site.urls),
 ]
--- pigments/views.py
+++ pigments/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Color
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the pigments index.")
+    colors = Color.objects.order_by('hex_code')
+    template = loader.get_template('colors/index.html')
+    context = {
+        'colors': colors,
+    }
+    return HttpResponse(template.render(context, request))
+
+def color(request, color_id):
+    return HttpResponse("You're looking at color %s." % color_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
