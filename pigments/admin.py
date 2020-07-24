a5699e42c2dbb4a44f4b26d0eaf5662615c90b7c
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-24 12:07:43 +0900

    models refactor

--- pigments/admin.py
+++ pigments/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Pigment, Color
+from .models import Pigment, Color, PigmentType
 
 admin.site.register(Pigment)
 admin.site.register(Color)
+admin.site.register(PigmentType)
--- pigments/models.py
+++ pigments/models.py
@@ -13,20 +13,12 @@ class Color(models.Model):
     return '{0} {1}'.format(self.name_ja, self.name_en) 
 
 class PigmentType(models.Model):
-  class Type(models.TextChoices):
-    TENNEN_IWA = 'TI', _('Tennen Iwaenogu 天然岩絵具')
-    SHIN_IWA = 'SI', _('Shin Iwaenogu　新岩絵具')
-    GOFUN = 'GO', _('Gofun 胡粉')
-    SUIHUI = 'SH', _('Suihi Enogu　水飛絵具')
-    TENNEN_TSUCHI = 'TT', _('Tennen Tsuchi Enogu　天然土絵具')
-
-  value = models.CharField(
-    max_length=2,
-    choices=Type.choices,
-    default=Type.TENNEN_IWA)
+  name_en = models.CharField(max_length=50)
+  name_ja = models.CharField(max_length=50)
+  description_en = models.CharField(max_length=200)
 
   def __str__(self):
-    return '{0}'.format(self.value.label) 
+    return '{0} {1}'.format(self.name_ja, self.name_en) 
 
 class Pigment(models.Model):
   color = models.ForeignKey(Color, on_delete=models.CASCADE)
