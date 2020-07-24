8c7a8df822c1dc7f78fad6427c748bebdf201cf1
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-25 00:01:28 +0900

    add links to root

--- root/views.py
+++ root/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.http import Http404
 from django.template import loader
 
 # Create your views here.
 
+from colors.models import Color, Pigment
+from categories.models import Category
+from grains.models import Grain
+
 def index(request):
-    return render(request, 'root/index.html', {})
+    colors = Color.objects.all()
+    categories = Category.objects.all()
+    grains = Grain.objects.all()
+    context = {'colors': colors, 'categories': categories, 'grains': grains}
+    return render(request, 'root/index.html', context)
