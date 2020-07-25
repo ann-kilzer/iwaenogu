a9be7a1f6e0d86f1cd866f866d215c95d012fb8c
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-26 00:07:33 +0900

    get the pigment and use the pigment color if possible

--- colors/views.py
+++ colors/views.py
@@ -26,11 +26,12 @@ def category_detail(request, color_id, category_code):
     category = Category.objects.get(pk=category_code)
     pigments = Pigment.objects.filter(color=color)
     grains= set([p.grain for p in pigments])
-    return render(request, 'colors/category_detail.html', {'color': color, 'category': category, 'grains': grains})
+    return render(request, 'colors/category_detail.html', {'color': color, 'category': category, 'grains': grains, 'pigments': pigments})
 
 def grain_detail(request, color_id, category_code, grain_id):
     color = get_object_or_404(Color, pk=color_id)
     category = Category.objects.get(pk=category_code)
     grain = Grain.objects.get(pk=grain_id)
     # TODO: get the pigment
-    return render(request, 'colors/grain_detail.html', {'color': color, 'category': category, 'grain': grain})
+    pigment = get_object_or_404(Pigment, color=color, grain=grain, category=category)
+    return render(request, 'colors/grain_detail.html', {'color': color, 'category': category, 'grain': grain, 'pigment': pigment})
