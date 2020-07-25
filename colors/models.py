9b115009dff022c6ff7b4a595624edb175a66874
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-25 21:56:52 +0900

    remove outdated hex_code field, adjust string print

--- colors/models.py
+++ colors/models.py
@@ -6,19 +6,20 @@ class Color(models.Model):
   name_en = models.CharField(max_length=50)
   name_ja = models.CharField(max_length=50)
   description_en = models.CharField(max_length=200)
-  hex_code = models.CharField(max_length=6)
   hex_color = ColorField(default='#ffffff')
 
-  def __str__(self):
+  def name(self):
     return '{0} {1}'.format(self.name_ja, self.name_en) 
+
+  def __str__(self):
+    return '{0} {1} {2}'.format(self.name_ja, self.name_en, self.hex_color) 
 
 class Pigment(models.Model):
   color = models.ForeignKey(Color, on_delete=models.CASCADE)
   category = models.ForeignKey('categories.Category', on_delete=models.CASCADE)
   grain = models.ForeignKey('grains.Grain', on_delete=models.CASCADE, )
-  hex_code = models.CharField(max_length=6)
   hex_color = ColorField(default='#ffffff')
   price = models.IntegerField(default=0)
 
   def __str__(self):
-    return '{0} {1}'.format(self.category, self.color) 
+    return '{0} {1} ({2}) {3}'.format(self.category, self.color.name(), self.grain, self.hex_color) 
