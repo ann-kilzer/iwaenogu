6792857922ebf19d780556923412ae772a163b80
Ann 杏 Kilzer <akilzer@gmail.com>
2021-09-11 19:16:12 +0900

    appease heroku 2

---
+++ colors/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Pigment, Color
+
+admin.site.register(Pigment)
+admin.site.register(Color)
---
+++ colors/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # ex: /colors/
+    path('', views.index, name='index'),
+    # ex: /colors/5/
+    path('<int:color_id>/', views.color, name='color'),
+    # ex: /colors/5/categories/
+    path('<int:color_id>/categories/', views.color, name='color'),
+    # ex: /colors/5/categories/3
+    path('<int:color_id>/categories/<str:category_code>/', views.category_detail, name='category_detail'),
+    # ex: /colors/5/categories/3/grains
+    path('<int:color_id>/categories/<str:category_code>/grains', views.category_detail, name='category_detail'),
+    # ex: /colors/5/categories/3/grains/5
+    path('<int:color_id>/categories/<str:category_code>/grains/<int:grain_id>', views.grain_detail, name='grain_detail'),
+]
