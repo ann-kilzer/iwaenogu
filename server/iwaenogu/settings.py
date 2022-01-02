65e9ed8d9cd522b03cf3bbfa2d47cb07499e1f1f
Ann 杏 Kilzer <akilzer@gmail.com>
2022-01-02 23:22:54 +0900

    remove postgres password

--- server/iwaenogu/settings.py
+++ server/iwaenogu/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'iwaenogu',
         'USER': 'otto',
-        'PASSWORD': 'letmeinnow',
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
