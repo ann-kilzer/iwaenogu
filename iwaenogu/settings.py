ce5b254fc97a5222cc7df716ae065cdd7bf1165d
Ann 杏 Kilzer <akilzer@gmail.com>
2022-01-02 23:03:42 +0900

    remove secret key from code, store as env var

--- iwaenogu/settings.py
+++ iwaenogu/settings.py
@@ -14,6 +14,9 @@ import os
 
 import django_heroku
 
+import environ
+environ.Env.read_env()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c8ux+h=aj#%r*c0^4-+2n9i(yfm%h^3$xt$6*do(v92xrxjju5'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
